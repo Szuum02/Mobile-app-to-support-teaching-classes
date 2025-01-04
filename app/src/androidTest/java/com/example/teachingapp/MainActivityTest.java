039e5f383213ce9c768157522088875c72102891
Szuum02 <s.urbanski2002@gmail.com>
2025-01-04 01:18:57 +0100

    Main activity test

--- app/src/androidTest/java/com/example/teachingapp/MainActivityTest.java
+++ app/src/androidTest/java/com/example/teachingapp/MainActivityTest.java
@@ -1,22 +1,20 @@
 package com.example.teachingapp;
 
+import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
-import androidx.test.core.app.ActivityScenario;
-import androidx.test.espresso.Espresso;
 import androidx.test.espresso.intent.Intents;
-import androidx.test.espresso.intent.matcher.IntentMatchers;
-import androidx.test.espresso.intent.rule.IntentsTestRule;
-import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
-
-import android.widget.Button;
 
 import com.example.teachingapp.User._Login;
 import com.example.teachingapp.User._RegistrationForm;
@@ -25,36 +23,32 @@ import com.example.teachingapp.User._RegistrationForm;
 public class MainActivityTest {
 
     @Rule
-    public IntentsTestRule<MainActivity> intentsTestRule =
-            new IntentsTestRule<>(MainActivity.class);
+    public ActivityScenarioRule<MainActivity> activityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
-    public void properlyRender() {
-        ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class);
-        scenario.onActivity(activity -> {
-            assertNotNull(activity);
+    public void testLoginButtonClick() {
+        Intents.init();
 
-            assertNotNull(activity.findViewById(R.id.welcome_image));
-            assertNotNull(activity.findViewById(R.id.welcome_text));
-            assertNotNull(activity.findViewById(R.id.info_text));
-            assertNotNull(activity.findViewById(R.id.login_button));
-            assertNotNull(activity.findViewById(R.id.register_button));
-        });
+        onView(withId(R.id.login_button)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.login_button)).perform(click());
+
+        intended(hasComponent(_Login.class.getName()));
+
+        Intents.release();
     }
 
     @Test
-    public void properlyHandleLoginButtonClick() {
-        ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class);
-        Espresso.onView(ViewMatchers.withId(R.id.login_button)).perform(click());
+    public void testRegisterButtonClick() {
+        Intents.init();
 
-        Intents.intended(IntentMatchers.hasComponent(_Login.class.getName()));
-    }
+        onView(withId(R.id.register_button)).check(matches(isDisplayed()));
 
-    @Test
-    public void properlyHandleRegisterButtonClick() {
-        ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class);
-        Espresso.onView(ViewMatchers.withId(R.id.register_button)).perform(click());
+        onView(withId(R.id.register_button)).perform(click());
 
-        Intents.intended(IntentMatchers.hasComponent(_RegistrationForm.class.getName()));
+        intended(hasComponent(_RegistrationForm.class.getName()));
+
+        Intents.release();
     }
 }
